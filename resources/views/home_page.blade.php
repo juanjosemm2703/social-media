d5499fd188ea7bfd9de7622388fc29dc43e70d60
juanjosemm2703 <www-data@s5280075.elf.ict.griffith.edu.au>
2023-08-23 01:14:43 +1000

    New feature: Styling post form

--- resources/views/home_page.blade.php
+++ resources/views/home_page.blade.php
@@ -8,9 +8,9 @@
     <div class="post_form">
         <form>
         <label for="fname">User Name:</label><br>
-        <input type="text" id="uname" name="uname" value="John"><br>
+        <input type="text" id="uname" name="uname" placeholder="Your name..."><br>
         <label for="lname">Post:</label><br>
-        <textarea id="post" name="post" rows="4" cols="50"></textarea><br>
+        <textarea id="post" name="post" rows="4" cols="50" placeholder="Start typing your post here..."></textarea><br>
         <input type="submit" value="Submit">
         </form>
     </div>
